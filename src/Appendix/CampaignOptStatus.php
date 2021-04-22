7e144b44d026ecb65bb8ac108e8858f499a93447
Dmitry Gladyshev <dgladyshev@promopult.ru>
2021-04-22 19:29:48 +0300

    Some constants

---
+++ src/Appendix/CampaignOptStatus.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Promopult\TikTokMarketingApi\Appendix;
+
+final class CampaignOptStatus
+{
+    public const ENABLED = 'ENABLED';
+    public const DISABLED = 'DISABLED';
+}
---
+++ src/Appendix/CampaignSecondaryStatus.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Promopult\TikTokMarketingApi\Appendix;
+
+/**
+ * Class CampaignSecondaryStatus
+ *
+ * @discard
+ */
+final class CampaignSecondaryStatus
+{
+    // Deleted
+    public const CAMPAIGN_STATUS_DELETE = 'CAMPAIGN_STATUS_DELETE';
+    // Advertiser review failed
+    public const CAMPAIGN_STATUS_ADVERTISER_AUDIT_DENY = 'CAMPAIGN_STATUS_ADVERTISER_AUDIT_DENY';
+    // Advertiser review in progress
+    public const CAMPAIGN_STATUS_ADVERTISER_AUDIT = 'CAMPAIGN_STATUS_ADVERTISER_AUDIT';
+    // Advertiser contract has not taken effect
+    public const ADVERTISER_CONTRACT_PENDING = 'ADVERTISER_CONTRACT_PENDING';
+    // Over budget
+    public const CAMPAIGN_STATUS_BUDGET_EXCEED = 'CAMPAIGN_STATUS_BUDGET_EXCEED';
+    // Suspended
+    public const CAMPAIGN_STATUS_DISABLE = 'CAMPAIGN_STATUS_DISABLE';
+    // Advertising in delivery
+    public const CAMPAIGN_STATUS_ENABLE = 'CAMPAIGN_STATUS_ENABLE';
+    // All statuses (including "Deleted")
+    public const CAMPAIGN_STATUS_ALL = 'CAMPAIGN_STATUS_ALL';
+    // All statuses (excluding "Deleted")
+    public const CAMPAIGN_STATUS_NOT_DELETE = 'CAMPAIGN_STATUS_NOT_DELETE';
+}
