0d4398cab5bf81bb280519c0284b8267602a0dbd
Dmitry Gladyshev <dgladyshev@promopult.ru>
2021-08-25 13:18:11 +0300

    Cs fix

--- src/Service/Report.php
+++ src/Service/Report.php
@@ -6,23 +6,22 @@ namespace Promopult\TikTokMarketingApi\Service;
 
 final class Report extends \Promopult\TikTokMarketingApi\AbstractService
 {
-
     /**
      * Create an Integrated Report
      *
      * @param int $advertiserId     Advertiser ID
-     * @param string $serviceType   Ad service type. Optional values: AUCTION, RESERVATION
      * @param string $reportType    Report type
      * @param array $dimensions     Grouping conditions. Auction and Reservation Ads support different dimensions.
-     * @param array $metrics        Metrics to query. Defaut: ["spend," "impressions"]
-     * @param ?string $startDate    Query start date, format such as: 2020-01-01, required when lifetime = false
-     * @param ?string $endDate      Query end date, format such as: 2020-01-01, required when lifetime = false
+     * @param ?string $dataLevel    Reporting data level. Required when report_type is BASIC,AUDIENCE or CATALOG
+     * @param ?string $serviceType
+     * @param ?array $metrics       Metrics to query. Defaut: ["spend," "impressions"]
      * @param bool $lifetime        Whether to request the lifetime metrics. The lifetime metric name is the same as
      *                              the normal one. If lifetime = true, the start_date and end_date parameters will
      *                              be ignored.
+     * @param ?string $startDate    Query start date, format such as: 2020-01-01, required when lifetime = false
+     * @param ?string $endDate      Query end date, format such as: 2020-01-01, required when lifetime = false
      * @param ?array $filters       Filter criteria. Supported filtering criteria vary according to 'service_type' and
      *                              'data_level'
-     * @param ?string $dataLevel    Reporting data level. Required when report_type is BASIC,AUDIENCE or CATALOG
      * @param ?string $orderField   Sort field. All supported metrics (excluding attribute metrics) support sorting,
      *                              not sorting by default
      * @param ?string $orderType    Sort. Optional value: ASC, DESC. Default value: DESC
@@ -32,7 +31,6 @@ final class Report extends \Promopult\TikTokMarketingApi\AbstractService
      * @return array
      *
      * @throws \Throwable
-     *
      * @see https://ads.tiktok.com/marketing_api/docs?id=1685752851588097
      */
     public function integratedGet(
--- tests/OAuth2ClientTest.php
+++ tests/OAuth2ClientTest.php
@@ -13,8 +13,14 @@ class OAuth2ClientTest extends TestCase
     public function testCreateAuthorizationUrl()
     {
         $expected = 'https://ads.tiktok.com/marketing_api/auth?app_id=appId&state=state&redirect_uri=redirectUri&scope=%5B1%2C2%5D';
+
         $actual = OAuth2Client::createAuthorizationUrl(
-            'appId', 'redirectUri', 'state', [1, 2], 'https://ads.tiktok.com');
+            'appId',
+            'redirectUri',
+            'state',
+            [1, 2],
+            'https://ads.tiktok.com'
+        );
 
         $partsToTest = [PHP_URL_HOST, PHP_URL_PATH, PHP_URL_QUERY, PHP_URL_SCHEME];
 
